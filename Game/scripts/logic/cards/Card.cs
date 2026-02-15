0f186357f4d7a96e332def083aa08edbd3bc74de
Theo van den Bogaart <todorus@todorus.com>
2026-02-15 22:39:57 +0100

    working new action format

--- Game/scripts/logic/cards/Card.cs
+++ Game/scripts/logic/cards/Card.cs
@@ -1,7 +1,9 @@
 using Godot;
+using Godot.Collections;
 using Lawfare.scripts.context;
 using Lawfare.scripts.logic.effects;
 using Lawfare.scripts.logic.@event;
+using Lawfare.scripts.logic.inputs;
 using Lawfare.scripts.subject;
 using Lawfare.scripts.subject.quantities;
 
@@ -12,7 +14,8 @@ public partial class Card(Action action) : GodotObject, IAction
     public string Label => action.Label;
 
     public Skill[] DicePools => action.DicePools;
-    
+    public Dictionary<InputLabel, EffectInput> Inputs => action.Inputs;
+
     public bool CanPerform(ISubject source) => action.CanPerform(source);
 
     public bool CanTarget(GameEvent gameEvent, ISubject target) => action.CanTarget(gameEvent, target);
